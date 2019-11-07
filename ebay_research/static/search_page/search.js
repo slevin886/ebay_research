fba5aabd1a35d8146713a8c96f7b541c6232f966
Scott Levin <slevin886@gmail.com>
2019-11-06 21:45:21 -0500

    changed items to return to radio buttons and moved table function own script

--- ebay_research/static/search_page/search.js
+++ ebay_research/static/search_page/search.js
@@ -3,6 +3,9 @@ import {
   plotSellerMap, plotSunBurst, plotTimeAvailable, plotTopSellers
 } from './plot_functions.js';
 
+import {drawTable} from "./table_function.js";
+
+
 // options for loading spinner
 const spinOptions = {
   lines: 13,
@@ -21,7 +24,7 @@ async function pullData() {
   let firstPull = true;
   let errorMessage = '';
   let showError = false;
-  let maxPages = formData.get('items_to_pull');
+  let maxPages = document.querySelector('input[name="pull_options"]:checked').value;
   let searchID;
   const target = document.getElementById('loading_spinner');
   let spinner = new Spinner(spinOptions).spin(target);
@@ -124,73 +127,3 @@ async function pullData() {
 window.onload = function () {
   document.getElementById('searchButton').addEventListener('click', pullData)
 };
-
-//creates clickable anchor tag for tabulator function
-const linkFormatter = function(cell, formatterParams){
-	let value = this.sanitizeHTML(cell.getValue()),
-	urlPrefix = formatterParams.urlPrefix || "",
-	label = this.emptyToSpace(value),
-	data;
-
-	if(formatterParams.labelField){
-		data = cell.getData();
-		label = data[formatterParams.labelField];
-	}
-
-	if(formatterParams.label){
-		switch(typeof formatterParams.label){
-			case "string":
-			label = formatterParams.label;
-			break;
-
-			case "function":
-			label = formatterParams.label(cell);
-			break;
-		}
-	}
-
-	if(formatterParams.urlField){
-		data = cell.getData();
-		value = data[formatterParams.urlField];
-	}
-
-	if(formatterParams.url){
-		switch(typeof formatterParams.url){
-			case "string":
-			value = formatterParams.url;
-			break;
-
-			case "function":
-			value = formatterParams.url(cell);
-			break;
-		}
-	}
-
-	return "<a href='" + urlPrefix + value + "' target='_blank'>" + label + "</a>";
-};
-
-
-function drawTable(tab_data) {
-	const table = new Tabulator("#tab_location", {
-			data: tab_data,           //load row data from array
-			layout: "fitColumns",      //fit columns to width of table
-			responsiveLayout: "hide",  //hide columns that dont fit on the table
-			tooltips: true,            //show tool tips on cells
-			initialSort: [{column: "watchCount", dir: "desc"}],
-			pagination: "local",       //paginate the data
-			paginationSize: 3,         //allow 7 rows per page of data
-			movableColumns: false,      //allow column order to be changed
-			resizableRows: false,       //allow row order to be changed
-			headerFilterPlaceholder: "search/filter...",
-			columns: [                 //define the table columns
-				{formatter: "rownum"},
-				{title: "Price", field: "currentPrice_value", formatter: "money", formatterParams: {symbol: "$"}},
-				{title: "Title", field: "title", formatter: "textarea", headerFilter: "input", width: 200,},
-				{title: "Image", field: "galleryURL", formatter: "image", width: 200},
-				{title: "Watch<br>Count", field: "watchCount"},
-				{title: "End Time", field: "endTime", width: 200, formatter: "textarea"},
-				{title: "View it!", field: "viewItemURL", formatter: linkFormatter, formatterParams: {'label': 'see on ebay'}},
-			],
-		}
-	)
-}
